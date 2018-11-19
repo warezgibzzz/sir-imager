2787680f5d7ed476cf51c5b3657eb2975afded76
Pavel S. Poronko <me@gibzzz.tk>
2018-11-19 17:38:55 +0700

    Routes

--- src/sir-imager.cr
+++ src/sir-imager.cr
@@ -1,12 +1,89 @@
 require "kemal"
+require "magickwand-crystal"
 
 macro serve(filename)
   render "src/views/#{{{filename}}}.ecr", "src/views/base/layout.ecr"
 end
 
+def get_dimensions(x : Int, y : Int, x1 : Int, y1 : Int)
+  puts x
+  puts y
+  puts x1
+  puts y1
+end
 
 get "/" do |env|
+  files = Dir.open(File.join [Kemal.config.public_folder, "uploads/"]).entries()
+  dirs = [] of String
+  
+  files.each do |file|
+    if File.directory?(File.join [Kemal.config.public_folder, "uploads/", File.basename(file)]) && file != "." && file != ".."
+      dirs << File.join ["/uploads/", File.basename(file)]
+    end
+  end
+  hello = "HELLO"
   serve "index"
+end
+
+post "/upload" do |env|
+  file = env.params.files["image"].tempfile
+  orig_name = env.params.files["image"].filename.as(String)
+  dir = env.params.body["dir"].as(String)
+  
+  file_path = ::File.join [Kemal.config.public_folder, "uploads/", dir, "/", orig_name]
+  File.open(file_path, "w") do |f|
+    IO.copy(file, f)
+  end
+  "Upload OK"
+end
+
+get "/:dir/:width/:height/:file" do |env|
+  file = env.params.url["file"].as(String)
+  dir = env.params.url["dir"].as(String)
+  width = env.params.url["width"].as(String).to_i
+  height = env.params.url["height"].as(String).to_i
+  file_path = ::File.join [Kemal.config.public_folder, "uploads/", dir, "/", file]
+
+
+  env.response.content_type = "image/jpeg"
+  LibMagick.magickWandGenesis
+  wand = LibMagick.newMagickWand
+  if LibMagick.magickReadImage( wand, file_path )
+    w = LibMagick.magickGetImageWidth wand
+    h = LibMagick.magickGetImageHeight wand
+    get_dimensions(w, h, width, height)
+    LibMagick.magickCropImage wand, w / 2, h / 2, LibMagick::FilterTypes::LanczosFilter, 1
+    ## or simply scale with:
+    # LibMagick.magickScaleImage wand, w / 2, h / 2
+    LibMagick.magickSetImageCompressionQuality wand, 80
+    LibMagick.magickWriteImage wand, File.basename(file_path)
+
+    buffer = LibMagick.magickGetImageBlob wand, out length
+    slice = Slice( UInt8 ).new( buffer, length )
+    LibMagick.magickWandTerminus
+    io = IO::Memory.new
+    io.write slice
+    io.to_s
+  else
+    "Not found"
+  end
+end
+
+get "/uploads/:path/" do |env|
+  path = env.params.url["path"]
+  if File.directory?(File.join [Kemal.config.public_folder, "uploads/", File.basename(path)]) != true
+    send_file env, File.join [Kemal.config.public_folder, "uploads/", File.basename(path)]
+  end
+
+  directory_entries = Dir.open(File.join [Kemal.config.public_folder, "uploads/", path]).entries()
+  files = [] of String
+  
+  directory_entries.each do |file|
+    if File.directory?(File.join [Kemal.config.public_folder, "uploads/", path, File.basename(file)]) != true && file != "." && file != ".."
+      files << File.join ["/uploads/", path, File.basename(file)]
+    end
+  end
+  serve "directory"
 end
 
 Kemal.run
